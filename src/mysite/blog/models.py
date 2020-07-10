7c8b8e0c0e102fd696315f4fd6a8d938739bbae3
Luis Calderon <luis.calderon@anahuac.mx>
2020-07-10 17:35:58 -0500

    changes on admin and models file

--- src/mysite/blog/models.py
+++ src/mysite/blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
                                 related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
                                 choices= STATUS_CHOICES,
